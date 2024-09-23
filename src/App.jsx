423eb3bab285df4562475bb59e3d93006384603d
TGFRT <68918738+TGFRT@users.noreply.github.com>
2024-09-23 10:08:17 -0500

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Vapi from "@vapi-ai/web";
 import { isPublicKeyMissingError } from "./utils";
 
 // Put your Vapi Public Key below.
-const vapi = new Vapi("0000XXXX-XXXX-XXXX-XXXX-XXXXXXXX0000");
+const vapi = new Vapi("ac5ebdd0-f122-4670-93c5-70b89c337ef7");
 
 const App = () => {
   const [connecting, setConnecting] = useState(false);
